72834369ee3b9eebff972ae4fbc59436fa7f6483
Nick Bansal <extreme_wired@msn.com>
2018-10-24 20:39:43 +0100

    Graphs done

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,9 +5,10 @@ const Category = ({ onChange }) => {
     return (
         <div>
             <select onChange={(e) => onChange(e.target.value)} name="category">
-                <option value="Genre">Please Select an option...</option>
+                <option value="">Please Select an option...</option>
                 <option value="Genre">Genre</option>
                 <option value="Author">Author</option>
+                <option value="Publisher">Publisher</option>
             </select>
         </div>
     )
--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -2,29 +2,31 @@ import React from 'react';
 import {Doughnut} from 'react-chartjs-2';
 
 const Graph = ({ category, booklist }) => {
-
    
     const newList = booklist.reduce((acc, value) => {
-        let newValue = value.volumeInfo.categories
+        let newValue
+        if (category === 'Genre') newValue = value.volumeInfo.categories
+        if (category === 'Author') newValue = value.volumeInfo.authors[0]
+        if (category === 'Publisher') newValue = value.volumeInfo.publisher
         acc[newValue] ? acc[newValue]++ : acc[newValue] = 1
         return acc
     }, {})
     
-    console.log(Array(newList.length).fill().map(item => '#'+ Math.floor(Math.random()*16777215).toString(16)))
+    const newArray = Array(booklist.length).fill(null)
 
     const data = {
         labels: Object.keys(newList),
         datasets: [{
             data: Object.values(newList),
-            backgroundColor: Array(newList.length).fill().map(item => '#'+ Math.floor(Math.random()*16777215).toString(16)),
-            hoverBackgroundColor: Array(newList.length).fill().map(item => '#'+ Math.floor(Math.random()*16777215).toString(16))
+            backgroundColor: newArray.map(item => '#' + Math.floor(Math.random()*16777215).toString(16)),
+            hoverBackgroundColor: newArray.map(item => '#' + Math.floor(Math.random()*16777215).toString(16))
         }]
     };
 
     return (
         <div>
-            <h2>Doughnut Example</h2>
-            <Doughnut data={data} />
+            {category && <h2>{category} Graph</h2>}
+            {category && <Doughnut data={data} />}
         </div>
     )
 }
